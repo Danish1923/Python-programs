c2e3dcdd35f5a0ac5dfd5e724efad3c82f96818d
Danish <kedieditz50@gmail.com>
2026-08-09 13:04:48 +0530

    Updated project

---
+++ areaofrectangle.py
@@ -0,0 +1,8 @@
+#Collecting user input
+
+length=float(input("Enter the Length:")) #store the length value
+width=float(input("Enter the width:")) #store the width value
+
+area=length*width #formula
+
+print(f"The area of the rectangle is {area:.2f}")
---
+++ areaoftriangle.py
@@ -0,0 +1,9 @@
+#collecting user input
+base=float(input("Enter the base value:"))
+height=float(input("Enter the height:"))
+
+area=0.5* base*height #formula (use 1/2 or 0.5 is same)
+
+#printing result
+
+print(f"The area of Triangle is {area:.2f}")
