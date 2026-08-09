192edefdaf781d53b0b17c65995f0db081919dfa
Danish <kedieditz50@gmail.com>
2026-08-09 13:44:33 +0530

    Added Fahrenheit to Celsius Program

---
+++ fahrenheittocelsius.py
@@ -0,0 +1,9 @@
+#collecting user input
+fahrenheit=float(input("Enter the Fahrenheit:"))
+
+#apply formula 
+celsius=(fahrenheit-32)* 5/9
+
+#display result
+print(f"The celsius of {fahrenheit} is {celsius:.2f}C")
+
