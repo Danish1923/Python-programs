e9c3b2ca9b014e3d6b244df1adf84160e72f982f
Danish <kedieditz50@gmail.com>
2026-08-10 11:27:33 +0530

    Added minutes to hours program

--- kmtomiles.py
+++ kmtomiles.py
@@ -5,4 +5,4 @@ km=float(input("Enter the Kilometer:"))
 mile=km*0.621371
 
 #display output
-print(f"The mile for your kilom eter is {mile:.2f}")
+print(f"The mile for your kilometer is {mile:.2f}")
---
+++ minutestohours.py
@@ -0,0 +1,8 @@
+#collecting user input
+minutes=int(input("Enter the minutes:"))
+
+#apply formula
+hours=minutes / 60
+
+#dispaly the output
+print(f"The Total no.of Hours is {hours:.2f}")
